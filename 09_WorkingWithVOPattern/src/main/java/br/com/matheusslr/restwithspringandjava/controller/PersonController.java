8c82cc9e14eda2fd4c0dba66643f46882c57121b
matheusslr <matheusslr@hotmail.com>
2023-01-26 14:09:49 -0300

    feat: Implementing VO Pattern with ModelMapper

---
+++ 09_WorkingWithVOPattern/src/main/java/br/com/matheusslr/restwithspringandjava/controller/PersonController.java
@@ -0,0 +1,49 @@
+package br.com.matheusslr.restwithspringandjava.controller;
+
+import br.com.matheusslr.restwithspringandjava.service.PersonService;
+import br.com.matheusslr.restwithspringandjava.vo.v1.PersonVO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/persons")
+public class PersonController {
+    private final PersonService personService;
+
+    public PersonController(PersonService personService) {
+        this.personService = personService;
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<PersonVO>> findAll() {
+        return ResponseEntity.ok(personService.findAll());
+    }
+
+    @GetMapping(path = "/{id}",
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<PersonVO> findById(@PathVariable(value = "id") Long id) {
+        return ResponseEntity.ok(personService.findById(id));
+    }
+
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE,
+            consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<PersonVO> save(@RequestBody PersonVO personVO) {
+        return new ResponseEntity<>(personService.save(personVO), HttpStatus.CREATED);
+    }
+
+    @PutMapping(produces = MediaType.APPLICATION_JSON_VALUE,
+            consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<PersonVO> update(@RequestBody PersonVO personVO) {
+        return new ResponseEntity<>(personService.update(personVO), HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable(value = "id") Long id) {
+        personService.remove(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
