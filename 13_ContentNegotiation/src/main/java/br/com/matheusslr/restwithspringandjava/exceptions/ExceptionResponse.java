165a26ce7a98162ef5e59ace2905d9d0f6bdf311
matheusslr <matheusslr@hotmail.com>
2023-01-28 10:32:23 -0300

    feat: Implementing Content Negotiation with JSON, XML and YAML

---
+++ 13_ContentNegotiation/src/main/java/br/com/matheusslr/restwithspringandjava/exceptions/ExceptionResponse.java
@@ -0,0 +1,25 @@
+package br.com.matheusslr.restwithspringandjava.exceptions;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Getter
+@Setter
+@Builder
+public class ExceptionResponse implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Date timestamp;
+    private String message;
+    private String details;
+
+    public ExceptionResponse(Date timestamp, String message, String details) {
+        this.timestamp = timestamp;
+        this.message = message;
+        this.details = details;
+    }
+}
